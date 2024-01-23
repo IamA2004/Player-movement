0cca057f95a7b9cab153dc5030237f6f2bebaae9
A <157512246+IamA2004@users.noreply.github.com>
2024-01-24 01:37:52 +0530

    Create Player.cs
    This script uses the KeyCode.Space to trigger the jump when the spacebar is pressed. Also, a basic check for grounding is implemented using a collision with a GameObject tagged as "Ground." Make sure to tag your ground GameObject accordingly for this to work

---
+++ Player.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private bool isGrounded;
+
+    void Update()
+    {
+        // Player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement);
+
+        // Player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if player is grounded
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
